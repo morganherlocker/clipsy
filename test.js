7558bcedd6a36a079ebbbbede488b6289512cd40
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-18 20:45:46 -0500

    working in node (need tests)

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var clipsy = require('./index')
 
 describe('clipsy', function(){
   it('should take a set of polygons and return a unioned polygon', function(){
-    
+    console.log(clipsy)
   })
 })
